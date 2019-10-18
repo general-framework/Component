b961ca60bf594349657749a368e66c85a8155870
j-j-m <martinjaco@gmail.com>
2019-10-18 18:44:50 -0400

    add component

--- Sources/Component/Alias.swift
+++ Sources/Component/Alias.swift
@@ -5,6 +5,8 @@
 //  Created by Jacob Martin on 10/18/19.
 //
 
+struct Component {}
+
 public typealias Effect = () -> Void
 
 public typealias Reducer<Value, Action> = (inout Value, Action) -> Effect
