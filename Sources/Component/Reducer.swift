9a4e7a962d50406ba8998cbe83124fdbc986c116
jacob martin <martinjaco@gmail.com>
2020-04-24 21:41:26 -0400

    update package

--- Sources/Component/Reducer.swift
+++ Sources/Component/Reducer.swift
@@ -1,47 +1,119 @@
-//
-//  Reducer.swift
-//  
-//
-//  Created by Jacob Martin on 10/18/19.
-//
+import CasePaths`
 
-public func combine<Value, Action>(
-    _ reducers: Reducer<Value, Action>...
-) -> Reducer<Value, Action> {
-    return { value, action in
-        let effects = reducers.map { $0(&value, action) }
-        return {
-            for effect in effects {
-                effect()
-            }
-        }
-    }
+//(inout RandomNumberGenerator) -> A
+struct Gen<A> {
+  let run: (inout RandomNumberGenerator) -> A
 }
 
-public func pullback<LocalValue, GlobalValue, LocalAction, GlobalAction>(
-    _ reducer: @escaping Reducer<LocalValue, LocalAction>,
-    value: WritableKeyPath<GlobalValue, LocalValue>,
-    action: WritableKeyPath<GlobalAction, LocalAction?>
-) -> Reducer<GlobalValue, GlobalAction> {
-    return { globalValue, globalAction in
-        guard let localAction = globalAction[keyPath: action] else { return {} }
-        let effect = reducer(&globalValue[keyPath: value], localAction)
-        return effect
-    }
+//(inout Substring) -> A?
+struct Parser<A> {
+  let run: (inout Substring) -> A?
 }
 
-public func logging<Value, Action>(
-    _ reducer: @escaping Reducer<Value, Action>
-) -> Reducer<Value, Action> {
-    return { value, action in
-        let effect = reducer(&value, action)
-        let newValue = value
-        return {
-            print("Action: \(action)")
-            print("Value:")
-            dump(newValue)
-            print("---")
-            effect()
-        }
-    }
+//(@escaping (A) -> Void) -> Void
+//struct Effect<A> {
+//  let run: (@escaping (A) -> Void) -> Void
+//}
+
+//public typealias Reducer<Value, Action, Environment> = (inout Value, Action, Environment) -> [Effect<Action>]
+public struct Reducer<Value, Action, Environment> {
+  let reducer: (inout Value, Action, Environment) -> [Effect<Action>]
+  
+  public init(_ reducer: @escaping (inout Value, Action, Environment) -> [Effect<Action>]) {
+    self.reducer = reducer
+  }
 }
+
+extension Reducer {
+  public func callAsFunction(_ value: inout Value, _ action: Action, _ environment: Environment) -> [Effect<Action>] {
+    self.reducer(&value, action, environment)
+  }
+}
+
+extension Reducer {
+  public static func combine(_ reducers: Reducer...) -> Reducer {
+    .init { value, action, environment in
+      let effects = reducers.flatMap { $0(&value, action, environment) }
+      return effects
+    }
+  }
+}
+
+//public func combine<Value, Action, Environment>(
+//  _ reducers: Reducer<Value, Action, Environment>...
+//) -> Reducer<Value, Action, Environment> {
+//  .init { value, action, environment in
+//    let effects = reducers.flatMap { $0(&value, action, environment) }
+//    return effects
+//  }
+//}
+
+extension Reducer {
+  public func pullback<GlobalValue, GlobalAction, GlobalEnvironment>(
+    value: WritableKeyPath<GlobalValue, Value>,
+    action: CasePath<GlobalAction, Action>,
+    environment: @escaping (GlobalEnvironment) -> Environment
+  ) -> Reducer<GlobalValue, GlobalAction, GlobalEnvironment> {
+    .init { globalValue, globalAction, globalEnvironment in
+      guard let localAction = action.extract(from: globalAction) else { return [] }
+      let localEffects = self(&globalValue[keyPath: value], localAction, environment(globalEnvironment))
+
+      return localEffects.map { localEffect in
+        localEffect.map(action.embed)
+          .eraseToEffect()
+      }
+    }
+  }
+}
+
+//public func pullback<LocalValue, GlobalValue, LocalAction, GlobalAction, LocalEnvironment, GlobalEnvironment>(
+//  _ reducer: Reducer<LocalValue, LocalAction, LocalEnvironment>,
+//  value: WritableKeyPath<GlobalValue, LocalValue>,
+//  action: CasePath<GlobalAction, LocalAction>,
+//  environment: @escaping (GlobalEnvironment) -> LocalEnvironment
+//) -> Reducer<GlobalValue, GlobalAction, GlobalEnvironment> {
+//  return .init { globalValue, globalAction, globalEnvironment in
+//    guard let localAction = action.extract(from: globalAction) else { return [] }
+//    let localEffects = reducer(&globalValue[keyPath: value], localAction, environment(globalEnvironment))
+//
+//    return localEffects.map { localEffect in
+//      localEffect.map(action.embed)
+//        .eraseToEffect()
+//    }
+//  }
+//}
+
+extension Reducer {
+  public func logging(
+    printer: @escaping (Environment) -> (String) -> Void = { _ in { print($0) } }
+  ) -> Reducer {
+    .init { value, action, environment in
+      let effects = self(&value, action, environment)
+      let newValue = value
+      let print = printer(environment)
+      return [.fireAndForget {
+        print("Action: \(action)")
+        print("Value:")
+        var dumpedNewValue = ""
+        dump(newValue, to: &dumpedNewValue)
+        print(dumpedNewValue)
+        print("---")
+        }] + effects
+    }
+  }
+}
+
+//public func logging<Value, Action, Environment>(
+//  _ reducer: Reducer<Value, Action, Environment>
+//) -> Reducer<Value, Action, Environment> {
+//  return .init { value, action, environment in
+//    let effects = reducer(&value, action, environment)
+//    let newValue = value
+//    return [.fireAndForget {
+//      print("Action: \(action)")
+//      print("Value:")
+//      dump(newValue)
+//      print("---")
+//      }] + effects
+//  }
+//}
--- Sources/Component/Store.swift
+++ Sources/Component/Store.swift
@@ -1,42 +1,105 @@
-//
-//  Store.swift
-//  
-//
-//  Created by Jacob Martin on 10/18/19.
-//
-
 import Combine
 
-public final class Store<Value, Action>: ObservableObject {
-  private let reducer: Reducer<Value, Action>
-  @Published public private(set) var value: Value
-  private var cancellable: Cancellable?
+public final class Store<Value, Action> {
+  private let reducer: Reducer<Value, Action, Any>
+  private let environment: Any
+  @Published private var value: Value
+  private var viewCancellable: Cancellable?
+  private var effectCancellables: Set<AnyCancellable> = []
 
-  public init(initialValue: Value, reducer: @escaping Reducer<Value, Action>) {
-    self.reducer = reducer
+  public init<Environment>(
+    initialValue: Value,
+    reducer: Reducer<Value, Action, Environment>,
+    environment: Environment
+  ) {
+    self.reducer = .init { value, action, environment in
+      reducer(&value, action, environment as! Environment)
+    }
     self.value = initialValue
+    self.environment = environment
   }
 
-  public func send(_ action: Action) {
-    let effect = self.reducer(&self.value, action)
-    effect()
+  private func send(_ action: Action) {
+    let effects = self.reducer(&self.value, action, self.environment)
+    effects.forEach { effect in
+      var effectCancellable: AnyCancellable?
+      var didComplete = false
+      effectCancellable = effect.sink(
+        receiveCompletion: { [weak self, weak effectCancellable] _ in
+          didComplete = true
+          guard let effectCancellable = effectCancellable else { return }
+          self?.effectCancellables.remove(effectCancellable)
+      },
+        receiveValue: { [weak self] in self?.send($0) }
+      )
+      if !didComplete, let effectCancellable = effectCancellable {
+        self.effectCancellables.insert(effectCancellable)
+      }
+    }
   }
 
-  public func view<LocalValue, LocalAction>(
+  public func scope<LocalValue, LocalAction>(
     value toLocalValue: @escaping (Value) -> LocalValue,
     action toGlobalAction: @escaping (LocalAction) -> Action
   ) -> Store<LocalValue, LocalAction> {
     let localStore = Store<LocalValue, LocalAction>(
       initialValue: toLocalValue(self.value),
-      reducer: { localValue, localAction in
+      reducer: .init { localValue, localAction, _ in
         self.send(toGlobalAction(localAction))
         localValue = toLocalValue(self.value)
-        return {}
-    }
+        return []
+    },
+      environment: self.environment
     )
-    localStore.cancellable = self.$value.sink { [weak localStore] newValue in
-      localStore?.value = toLocalValue(newValue)
-    }
+    localStore.viewCancellable = self.$value
+      .map(toLocalValue)
+      .sink { [weak localStore] newValue in
+        localStore?.value = newValue
+      }
     return localStore
+  }
+}
+
+@dynamicMemberLookup
+public final class ViewStore<Value, Action>: ObservableObject {
+  @Published public fileprivate(set) var value: Value
+  fileprivate var cancellable: Cancellable?
+  public let send: (Action) -> Void
+  
+  public subscript<LocalValue>(dynamicMember keyPath: KeyPath<Value, LocalValue>) -> LocalValue {
+    self.value[keyPath: keyPath]
+  }
+
+  public init(
+    initialValue value: Value,
+    send: @escaping (Action) -> Void
+  ) {
+    self.value = value
+    self.send = send
+  }
+}
+
+extension Store where Value: Equatable {
+  public var view: ViewStore<Value, Action> {
+    self.view(removeDuplicates: ==)
+  }
+}
+
+extension Store {
+  public func view(
+    removeDuplicates predicate: @escaping (Value, Value) -> Bool
+  ) -> ViewStore<Value, Action> {
+    let viewStore = ViewStore(
+      initialValue: self.value,
+      send: self.send
+    )
+
+    viewStore.cancellable = self.$value
+      .removeDuplicates(by: predicate)
+      .sink(receiveValue: { [weak viewStore] value in
+        viewStore?.value = value
+      })
+
+    return viewStore
   }
 }
